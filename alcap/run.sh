34426c839694f4fb0f4d24b72f90efb17d940aaf
John R Quirk <jrquirk@bu.edu>
2018-09-21 15:58:38 -0400

    Bunch of my macros

---
+++ alcap/run.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+# Arguments
+# Multitask macro name. Scripts searched for are $1_#.mac,
+#   where # is the task number.
+# Output filname prefix. Saved to output/$2_#.root
+# Number of parallel tasks.
+
+mkdir -p macros/parallel/
+macro=$(basename $1)
+for i in $(seq $3); do
+	cp $1 macros/parallel/$macro.$i
+	rnd1=$(od -vAn -N4 -tu4 < /dev/urandom)
+	rnd2=$(od -vAn -N4 -tu4 < /dev/urandom)
+	sed -i "s/setSeeds .*/setSeeds $rnd1 $rnd2/" macros/parallel/$macro.$i
+done
+seq $3 | parallel --eta --keep-order --jobs 12 --results output/jobs \
+	"OFILENAMEROOT=output/$2_{}.root; g4sim macros/parallel/$macro.{}"
