5cff267fe5894430dcd56f30efecf0ad9a1221dc
John R Quirk <jrquirk@bu.edu>
2019-09-03 17:01:30 -0400

    Fixed some errors related to upgrading to a new version of GEANT4. Specifically FindROOT.cmake was removed in new G4 versions, so tell cmake to check current directory (also changed it to a required component). Otherwise changed CLHEP::HepRandom references to G4Random as on the 10.00p3 patch. Also changed the random engine to JamesRandom, which I think is what Mu2e uses, and is the default.

--- alcap/run_jq.sh
+++ alcap/run_jq.sh
@@ -9,9 +9,10 @@ mkdir -p macros/parallel/
 macro=$(basename $1)
 for i in $(seq $3); do
 	cp $1 macros/parallel/$macro.$i
-	rnd1=$(od -vAn -N4 -tu4 < /dev/urandom)
-	rnd2=$(od -vAn -N4 -tu4 < /dev/urandom)
-	sed -i "s/setSeeds .*/setSeeds $rnd1 $rnd2/" macros/parallel/$macro.$i
+	rnd=$(od -vAn -N4 -td4 < /dev/urandom)
+	sed -i "s/setSeeds .*/setSeeds $rnd/" macros/parallel/$macro.$i
 done
+
+mkdir -p work
 seq $3 | parallel --eta --results output/jobs \
-	"OFILENAMEROOT=output/$2_{}.root; g4sim macros/parallel/$macro.{}"
+	"cd \$(mktemp -d work/{}.XXXXX); OFILENAMEROOT=$ALCAPWORKROOT/output/$2.{}.root; g4sim $ALCAPWORKROOT/macros/parallel/$macro.{}"
